dd3d674cfb18660725bf0a3420a4908ab5121695
danielgarza <danielgarza22@gmail.com>
2022-01-09 02:31:28 -0500

    add more changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,18 @@
+# Daniel Garza Homework
+
+# Imported modules
 import os
 import csv
 
+# Assign a name to the path where the csv file is located
 budget_csv = os.path.join("Resources","budget_data.csv")
 
+# Open and read csv file
 with open(budget_csv) as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader)
 
+    # Create variables
     months = 0
     total = 0
     prev_rev = 0
@@ -14,12 +20,13 @@ with open(budget_csv) as csv_file:
     inc = ['',0]
     dec = ['',0]
 
+    # Created a loop
     for i,row in enumerate(csv_reader):
         rev = int(row[1])
         months += 1
         total += rev
 
-        # Avg Change
+        # Average Change
         change = rev - prev_rev
         prev_rev = rev
         if i == 0:
@@ -31,11 +38,13 @@ with open(budget_csv) as csv_file:
             inc[0] = row[0]
             inc[1] = change
 
+        #  Greatest decrease
         if change < dec[1]:
             dec[0] = row[0]
             dec[1] = change
 
-output = f'''
+# Create analysis report
+analysis_output = f'''
 Financial Analysis
 ----------------------------
 Total Months: {months}
@@ -44,8 +53,10 @@ Average  Change: ${total_ch/(months-1)}
 Greatest Increase in Profits: {inc[0]} (${inc[1]:,})
 Greatest Decrease in Profits: {dec[0]} (${dec[1]:,})'''
 
-print(output)
+#print analysis report
+print(analysis_output)
 
+# Create a file and save PyBank Analysis to a file
 analysis_file = os.path.join("Analysis","PyBank_Analysis.txt")
 with open(analysis_file,"a") as f:
     print(output, file=f)
